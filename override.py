708a0b0887d8b16331af0d00bbe13f948c81e10c
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-02-27 20:42:43 +0100

    Owner utility commands

---
+++ override.py
@@ -0,0 +1,66 @@
+"""General owner utility commands."""
+import logging
+import asyncio
+import os
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from variables import Config
+from extchecks import is_owner
+
+class Overrides(commands.GroupCog, name="override", description="Owner override commands."):
+    """Owner override commands."""
+    def __init__(self, bot: commands.Bot, config: Config):
+        self._bt = bot
+        self._config = config
+        super().__init__()
+        logging.info("Loaded %s", self.__class__.__name__)
+
+    @app_commands.command(name="reload", description="Reloads the bot's cogs.")
+    @app_commands.check(is_owner)
+    async def reload(self, ctx: discord.Interaction):
+        """Reloads the bot's cogs."""
+        await ctx.response.send_message("Reloading cogs...")
+        logging.info("Reloading cogs...")
+        await self._bt.reload_extension("main_utils")
+        await self._bt.reload_extension("settings")
+        await self._bt.reload_extension("override")
+        await ctx.channel.send("Reloaded cogs.") #type: ignore
+        logging.info("Finished reloading cogs.")
+
+    @app_commands.command(name="restart", description="Restarts the bot.")
+    @app_commands.check(is_owner)
+    async def restart(self, ctx: discord.Interaction):
+        """Restarts the bot."""
+        await ctx.response.send_message("Restarting...")
+        logging.info("Restarting...")
+        await self._bt.close()
+
+    @app_commands.command(name="pull", description="Pulls the latest changes from the git repo.")
+    @app_commands.check(is_owner)
+    async def pull(self, ctx: discord.Interaction):
+        """Pulls the latest changes from the git repo."""
+        await ctx.response.send_message("Pulling latest changes...")
+        logging.info("Pulling latest changes...")
+        pull = await asyncio.create_subprocess_shell("git pull",
+                                                    stdout=asyncio.subprocess.PIPE,
+                                                    stderr=asyncio.subprocess.PIPE,
+                                                    cwd=os.path.dirname(os.path.realpath(__file__)))
+        stdo, stdr = await pull.communicate()
+        if stdo:
+            await ctx.followup.send(f'[stdout]\n{stdo.decode()}')
+            logging.info('[stdout]\n%s', stdo.decode())
+
+        if stdr:
+            await ctx.followup.send(f'[stderr]\n{stdr.decode()}')
+            logging.info('[stderr]\n%s', stdr.decode())
+
+        await ctx.followup.send("Finished pulling latest changes.\n"
+                                "Restart bot or reload cogs to apply changes.")
+
+
+async def setup(bot: commands.Bot):
+    """Setup function for the cog."""
+    await bot.add_cog(Overrides(bot, Config(bot)))
--- start_bot.py
+++ start_bot.py
@@ -2,11 +2,9 @@
 import logging
 
 import discord
-from discord import app_commands
 from discord.ext import commands
 
 from variables import Config, VERSION, intents, handler, Secret
-from extchecks import is_owner
 
 bot = commands.Bot(command_prefix='~', intents=intents)
 cnfg = Config(bot)
@@ -23,19 +21,9 @@ async def on_ready():
     logging.info("Discord.py version: %s", discord.__version__)
     await bot.load_extension("main_utils")
     await bot.load_extension("settings")
+    await bot.load_extension("override")
     await bot.tree.sync()
     logging.info("Finished loading cogs.")
-
-@bot.tree.command(name="reload", description="Reloads the bot's cogs.")
-@app_commands.check(is_owner)
-async def reload(ctx: discord.Interaction):
-    '''Reloads the bot's cogs.'''
-    await ctx.response.send_message("Reloading cogs...")
-    logging.info("Reloading cogs...")
-    await bot.reload_extension("main_utils")
-    await bot.reload_extension("settings")
-    await ctx.channel.send("Reloaded cogs.") #type: ignore
-    logging.info("Finished reloading cogs.")
 
 if __name__ == "__main__":
     bot.run(scrt.token, log_handler=handler, root_logger=True)
