29c391cd708bbb6de9137b715a645b8ddf2f1d9a
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-03-08 20:56:47 +0100

    Main Utils config dbdated

--- ticket_plus/cogs/main_utils.py
+++ ticket_plus/cogs/main_utils.py
@@ -2,12 +2,17 @@
 import asyncio
 import logging
 import re
+from string import Template
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from sqlalchemy.orm import selectinload
 
+from ticket_plus import TicketPlus
+from ticket_plus.database.models import Guild
 from ticket_plus.database.statvars import VERSION, Config
+from ticket_plus.ext.checks import is_staff_check
 
 CONFG = Config("offline")
 
@@ -15,8 +20,7 @@ CONFG = Config("offline")
 class Utility(commands.Cog, name="Main Utilities"):
     """Main bot utilities"""
 
-    def __init__(self, bot: commands.Bot, config: Config):
-        self._config = config
+    def __init__(self, bot: TicketPlus):
         self._bt = bot
         logging.info("Loaded %s", self.__class__.__name__)
 
@@ -31,54 +35,73 @@ class Utility(commands.Cog, name="Main Utilities"):
         Please ensure you add your Ticket bot ID or approved
         Ticket invoking user IDs to the config file or via commands.
         """
-        if isinstance(channel, discord.channel.TextChannel):
-            gld = channel.guild
-            cnfg = self._config
-            async for entry in gld.audit_logs(
-                limit=3, action=discord.AuditLogAction.channel_create
-            ):
-                if entry.user is None:
-                    continue
-                if entry.target == channel and entry.user.id in cnfg.ticket_users:
-                    nts_thrd: discord.Thread = await channel.create_thread(
-                        name="Staff Notes",
-                        reason=f"Staff notes for Ticket {channel.name}",
-                        auto_archive_duration=10080,
+        async with self._bt.get_connection() as confg:
+            if isinstance(channel, discord.channel.TextChannel):
+                gld = channel.guild
+                async for entry in gld.audit_logs(
+                    limit=3, action=discord.AuditLogAction.channel_create
+                ):
+                    if entry.user is None:
+                        continue
+                    guild = await confg.get_guild(
+                        gld.id,
+                        (
+                            selectinload(Guild.ticket_users),
+                            selectinload(Guild.observers_roles),
+                        ),
                     )
-                    await nts_thrd.send(
-                        cnfg.open_msg.safe_substitute(channel=channel.mention)
-                    )
-                    logging.info(
-                        "Created thread %s for %s", nts_thrd.name, channel.name
-                    )
-                    if self._config.observers:
-                        inv = await nts_thrd.send(
-                            " ".join([role.mention for role in cnfg.observers])
+                    guild_bt = self._bt.get_guild(gld.id)
+                    ticket_user_ids = guild.get_id_list("ticket_users", "user_id")
+                    if entry.target == channel and entry.user.id in ticket_user_ids:
+                        nts_thrd: discord.Thread = await channel.create_thread(
+                            name="Staff Notes",
+                            reason=f"Staff notes for Ticket {channel.name}",
+                            auto_archive_duration=10080,
                         )
-                        await inv.delete()
-                    if self._config.strip_buttons:
-                        await asyncio.sleep(1)
-                        async for msg in channel.history(oldest_first=True, limit=2):
-                            if msg.author.id in cnfg.ticket_users:
-                                await channel.send(embeds=msg.embeds)
-                                await msg.delete()
-                    if self._config.community_roles:
-                        overwrite = discord.PermissionOverwrite()
-                        overwrite.view_channel = True
-                        overwrite.add_reactions = True
-                        overwrite.send_messages = True
-                        overwrite.read_messages = True
-                        overwrite.read_message_history = True
-                        overwrite.attach_files = True
-                        overwrite.embed_links = True
-                        overwrite.use_application_commands = True
-                        for role in self._config.community_roles:
-                            try:
-                                await channel.set_permissions(
-                                    role, overwrite=overwrite
-                                )  # type: ignore
-                            except TypeError:
-                                pass
+                        await nts_thrd.send(
+                            Template(guild.open_message).safe_substitute(
+                                channel=channel.mention
+                            )
+                        )
+                        logging.info(
+                            "Created thread %s for %s", nts_thrd.name, channel.name
+                        )
+                        if guild.observers_roles:
+                            observer_ids = guild.get_id_list(
+                                "observers_roles", "role_id"
+                            )
+                            inv = await nts_thrd.send(
+                                " ".join([f"<@&{role_id}>" for role_id in observer_ids])
+                            )
+                            await inv.delete()
+                        if guild.strip_buttons:
+                            await asyncio.sleep(1)
+                            async for msg in channel.history(
+                                oldest_first=True, limit=2
+                            ):
+                                if msg.author.id in ticket_user_ids:
+                                    await channel.send(embeds=msg.embeds)
+                                    await msg.delete()
+                        if guild.community_roles:
+                            comm_roles = guild.get_id_list("community_roles", "role_id")
+                            overwrite = discord.PermissionOverwrite()
+                            overwrite.view_channel = True
+                            overwrite.add_reactions = True
+                            overwrite.send_messages = True
+                            overwrite.read_messages = True
+                            overwrite.read_message_history = True
+                            overwrite.attach_files = True
+                            overwrite.embed_links = True
+                            overwrite.use_application_commands = True
+                            for role in comm_roles:
+                                try:
+                                    rle: discord.Role = guild_bt.get_role(role)  # type: ignore
+                                    await channel.set_permissions(
+                                        rle, overwrite=overwrite
+                                    )
+                                except (TypeError, discord.NotFound):
+                                    pass
+            await confg.close()
 
     @commands.Cog.listener(name="on_message")
     async def on_message(self, message: discord.Message) -> None:
@@ -86,26 +109,29 @@ class Utility(commands.Cog, name="Main Utilities"):
         Minor Extension: Message Discovery
         Displays the message linked to as an embed.
         """
-        if message.author.bot:
-            return
-        if self._bt.intents.message_content:
-            alpha = re.search(
-                r"https:\/\/(?:canary\.)?discord\.com\/channels\/(?P<srv>\d{18})\/(?P<cha>\d{18})\/(?P<msg>\d*)",  # skipcq: FLK-E501 # pylint: disable=line-too-long
-                message.content,
-            )  # pylint: disable=line-too-long
-            if alpha:
-                try:
-                    chan = self._bt.get_guild(int(alpha.group("srv"))).get_channel_or_thread(int(alpha.group("cha")))  # type: ignore pylint: disable=line-too-long
-                    got_msg = await chan.fetch_message(int(alpha.groupdict()["msg"]))  # type: ignore # pylint: disable=line-too-long
-                except:  # pylint: disable=bare-except
-                    return
-                data = discord.Embed(description=got_msg.content, color=0x0D0EB4)
-                data.set_author(name=got_msg.author.name, icon_url=got_msg.author.avatar.url)  # type: ignore # pylint: disable=line-too-long
-                data.set_footer(text=f"Sent in {got_msg.channel.name} at {got_msg.created_at}")  # type: ignore # pylint: disable=line-too-long
-                data.set_image(
-                    url=got_msg.attachments[0].url if got_msg.attachments else None
-                )
-                await message.reply(embed=data)
+        async with self._bt.get_connection() as confg:
+            if message.author.bot or message.guild is None:
+                return
+            guild = await confg.get_guild(message.guild.id)
+            if guild.msg_discovery:
+                alpha = re.search(
+                    r"https:\/\/(?:canary\.)?discord\.com\/channels\/(?P<srv>\d{18})\/(?P<cha>\d{18})\/(?P<msg>\d*)",  # skipcq: FLK-E501 # pylint: disable=line-too-long
+                    message.content,
+                )  # pylint: disable=line-too-long
+                if alpha:
+                    try:
+                        chan = self._bt.get_guild(int(alpha.group("srv"))).get_channel_or_thread(int(alpha.group("cha")))  # type: ignore pylint: disable=line-too-long
+                        got_msg = await chan.fetch_message(int(alpha.groupdict()["msg"]))  # type: ignore # pylint: disable=line-too-long
+                    except:  # pylint: disable=bare-except
+                        return
+                    data = discord.Embed(description=got_msg.content, color=0x0D0EB4)
+                    data.set_author(name=got_msg.author.name, icon_url=got_msg.author.avatar.url)  # type: ignore # pylint: disable=line-too-long
+                    data.set_footer(text=f"Sent in {got_msg.channel.name} at {got_msg.created_at}")  # type: ignore # pylint: disable=line-too-long
+                    data.set_image(
+                        url=got_msg.attachments[0].url if got_msg.attachments else None
+                    )
+                    await message.reply(embed=data)
+            await confg.close()
 
     @app_commands.command(
         name="ping", description="The classic ping command. Checks the bot's latency."
@@ -131,36 +157,39 @@ class Utility(commands.Cog, name="Main Utilities"):
 
     @app_commands.command(name="respond", description="Respond to a ticket as the bot.")
     @app_commands.guild_only()
-    @app_commands.checks.has_any_role(*CONFG.staff_ids)
+    @is_staff_check()
     @app_commands.describe(message="The message to send to the ticket.")
     async def respond(self, ctx: discord.Interaction, message: str):
         """
         EXTENSION 2: Anonymised staff responses.
         This command is used to respond to a ticket as the bot.
         """
-        if isinstance(ctx.channel, discord.Thread):
-            if isinstance(ctx.channel.parent, discord.TextChannel):
+        async with self._bt.get_connection() as confg:
+            guild = await confg.get_guild(ctx.guild.id)  # type: ignore # Already checked in is_staff_check
+            if isinstance(ctx.channel, discord.Thread):
+                if isinstance(ctx.channel.parent, discord.TextChannel):
+                    await ctx.response.send_message(
+                        "Responding to ticket with message:\n" + message
+                    )
+                    await ctx.channel.parent.send(
+                        f"**{guild.staff_team_name}:** " + message
+                    )
+                else:
+                    await ctx.response.send_message(
+                        "Cannot respond to forum channel.", ephemeral=True
+                    )
+                return
+            if isinstance(ctx.channel, discord.TextChannel):
                 await ctx.response.send_message(
-                    "Responding to ticket with message:\n" + message
+                    "Anonymously responding to ticket with message:\n" + message,
+                    ephemeral=True,
                 )
-                await ctx.channel.parent.send(
-                    f"**{self._config.staff_team}:** " + message
-                )
-            else:
-                await ctx.response.send_message(
-                    "Cannot respond to forum channel.", ephemeral=True
-                )
-            return
-        if isinstance(ctx.channel, discord.TextChannel):
-            await ctx.response.send_message(
-                "Anonymously responding to ticket with message:\n" + message,
-                ephemeral=True,
-            )
-            await ctx.channel.send(f"**{self._config.staff_team}:** " + message)
+                await ctx.channel.send(f"**{guild.staff_team_name}:** " + message)
+            await confg.close()
 
     @app_commands.command(name="join", description="Join a ticket's staff notes.")
     @app_commands.guild_only()
-    @app_commands.checks.has_any_role(*CONFG.staff_ids)
+    @is_staff_check()
     async def join(self, ctx: discord.Interaction):
         """
         EXTENSION 3: Staff notes.
@@ -183,8 +212,6 @@ class Utility(commands.Cog, name="Main Utilities"):
         )
 
 
-async def setup(bot: commands.Bot):
+async def setup(bot: TicketPlus):
     """Setup function for the cog."""
-    global CONFG  # pylint: disable=global-statement
-    CONFG = getattr(bot, "config", Config(bot))
-    await bot.add_cog(Utility(bot, CONFG))
+    await bot.add_cog(Utility(bot))
