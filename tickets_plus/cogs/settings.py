3fedb8f9d7aecfef2af260abdf85afaa5942913e
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-03-09 20:31:18 +0100

    dbtized settings. moved for categorization

--- tickets_plus/cogs/settings.py
+++ tickets_plus/cogs/settings.py
@@ -25,19 +25,16 @@ class Settings(commands.GroupCog, name="settings", description="Settings for the
         This command is used to change the tracked users.
         If a user is already tracked, they will be untracked.
         """
-        if ctx.guild is None:
-            await ctx.response.send_message("This command can only be used in a guild.")
-            return
         async with self._bt.get_connection() as conn:
-            rspns = ctx.response
-            new, ticket_user = await conn.get_ticket_user(user.id, ctx.guild.id, False)
+            new, ticket_user = await conn.get_ticket_user(user.id, ctx.guild.id)  # type: ignore
             if not new:
                 await conn.delete(ticket_user)
-                await rspns.send_message(f"Untracked {user.mention}", ephemeral=True)
+                text = f"Untracked {user.mention}"
             else:
-                await rspns.send_message(f"Tracked {user.mention}", ephemeral=True)
+                text = f"Tracked {user.mention}"
             await conn.commit()
             await conn.close()
+        await ctx.response.send_message(text, ephemeral=True)
 
     @app_commands.command(name="staff", description="Change the staff roles.")
     @app_commands.describe(role="The role to add/remove from staff roles.")
@@ -46,19 +43,16 @@ class Settings(commands.GroupCog, name="settings", description="Settings for the
         This command is used to change the staff roles, Staff are allowed to use staff commands.
         If a role is already here, it will be removed.
         """
-        rspns = ctx.response
-        stff = self._config.staff
-        if role in stff:
-            stff.remove(role)
-            await rspns.send_message(
-                f"Removed {role.mention} from staff roles.", ephemeral=True
-            )
-        else:
-            stff.append(role)
-            await rspns.send_message(
-                f"Added {role.mention} to staff roles.", ephemeral=True
-            )
-        self._config.staff = stff
+        async with self._bt.get_connection() as conn:
+            new, staff_role = await conn.get_staff_role(role.id, ctx.guild.id)  # type: ignore
+            if not new:
+                await conn.delete(staff_role)
+                text = f"Removed {role.mention} from staff roles."
+            else:
+                text = f"Added {role.mention} to staff roles."
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(text, ephemeral=True)
 
     @app_commands.command(name="observers", description="Change the observers roles.")
     @app_commands.describe(role="The role to add/remove from observers roles.")
@@ -67,54 +61,16 @@ class Settings(commands.GroupCog, name="settings", description="Settings for the
         This command is used to change the observers roles, which are pinged one new notes threads.
         If a role is already here, it will be removed.
         """
-        rspns = ctx.response
-        obsrvrs = self._config.observers
-        if role in obsrvrs:
-            obsrvrs.remove(role)
-            await rspns.send_message(
-                f"Removed {role.mention} from ping staff roles.", ephemeral=True
-            )
-        else:
-            obsrvrs.append(role)
-            await rspns.send_message(
-                f"Added {role.mention} to ping staff roles.", ephemeral=True
-            )
-        self._config.observers = obsrvrs
-
-    @app_commands.command(name="openmsg", description="Change the open message.")
-    @app_commands.describe(message="The new open message.")
-    async def change_openmsg(self, ctx: discord.Interaction, message: str):
-        """This command is used to change the open message."""
-        self._config.open_msg = message
-        await ctx.response.send_message(
-            f"Open message is now {message}", ephemeral=True
-        )
-
-    @app_commands.command(name="staffteam", description="Change the staff team's name.")
-    @app_commands.describe(name="The new staff team's name.")
-    async def change_staffteam(self, ctx: discord.Interaction, name: str):
-        """This command is used to change the staff team's name."""
-        self._config.staff_team = name
-        await ctx.response.send_message(f"Staff team is now {name}", ephemeral=True)
-
-    @app_commands.command(
-        name="msgdiscovery", description="Toggle message link discovery."
-    )
-    async def toggle_msg_discovery(self, ctx: discord.Interaction):
-        """This command is used to toggle message link discovery."""
-        self._config.msg_discovery = not self._config.msg_discovery
-        await ctx.response.send_message(
-            f"Message link discovery is now {self._config.msg_discovery}",
-            ephemeral=True,
-        )
-
-    @app_commands.command(name="stripbuttons", description="Toggle button stripping.")
-    async def toggle_button_stripping(self, ctx: discord.Interaction):
-        """This command is used to toggle button stripping."""
-        self._config.strip_buttons = not self._config.strip_buttons
-        await ctx.response.send_message(
-            f"Button stripping is now {self._config.strip_buttons}", ephemeral=True
-        )
+        async with self._bt.get_connection() as conn:
+            new, obsrvrs = await conn.get_staff_role(role.id, ctx.guild.id)  # type: ignore
+            if not new:
+                await conn.delete(obsrvrs)
+                text = f"Removed {role.mention} from ping staff roles."
+            else:
+                text = f"Added {role.mention} to ping staff roles."
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(text, ephemeral=True)
 
     @app_commands.command(
         name="communitysupport", description="Change the community support roles."
@@ -128,19 +84,72 @@ class Settings(commands.GroupCog, name="settings", description="Settings for the
         COMSUP roles are added to channels side-by-side without any perms.
         If a role is already here, it will be removed.
         """
-        rspns = ctx.response
-        comsup = self._config.community_roles
-        if role in comsup:
-            comsup.remove(role)
-            await rspns.send_message(
-                f"Removed {role.mention} from community support roles.", ephemeral=True
-            )
-        else:
-            comsup.append(role)
-            await rspns.send_message(
-                f"Added {role.mention} to community support roles.", ephemeral=True
-            )
-        self._config.community_roles = comsup
+        async with self._bt.get_connection() as conn:
+            new, comsup = await conn.get_community_role(role.id, ctx.guild.id)  # type: ignore
+            if not new:
+                await conn.delete(comsup)
+                text = f"Removed {role.mention} from community support roles."
+            else:
+                text = f"Added {role.mention} to community support roles."
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(text, ephemeral=True)
+
+    @app_commands.command(name="openmsg", description="Change the open message.")
+    @app_commands.describe(message="The new open message.")
+    async def change_openmsg(self, ctx: discord.Interaction, message: str):
+        """This command is used to change the open message."""
+        async with self._bt.get_connection() as conn:
+            guild = await conn.get_guild(ctx.guild.id)  # type: ignore
+            guild.open_message = message
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(
+            f"Open message is now {message}", ephemeral=True
+        )
+
+    @app_commands.command(
+        name="staffteamname", description="Change the staff team's name."
+    )
+    @app_commands.describe(name="The new staff team's name.")
+    async def change_staffteamname(self, ctx: discord.Interaction, name: str):
+        """This command is used to change the staff team's name."""
+        async with self._bt.get_connection() as conn:
+            guild = await conn.get_guild(ctx.guild.id)  # type: ignore
+            guild.staff_team_name = name
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(f"Staff team is now {name}", ephemeral=True)
+
+    @app_commands.command(
+        name="msgdiscovery", description="Toggle message link discovery."
+    )
+    async def toggle_msg_discovery(self, ctx: discord.Interaction):
+        """This command is used to toggle message link discovery."""
+        async with self._bt.get_connection() as conn:
+            guild = await conn.get_guild(ctx.guild.id)  # type: ignore
+            new_status = not guild.msg_discovery
+            guild.msg_discovery = new_status
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(
+            f"Message link discovery is now {new_status}",
+            ephemeral=True,
+        )
+
+    @app_commands.command(name="stripbuttons", description="Toggle button stripping.")
+    async def toggle_button_stripping(self, ctx: discord.Interaction):
+        """This command is used to toggle button stripping."""
+        async with self._bt.get_connection() as conn:
+            guild = await conn.get_guild(ctx.guild.id)  # type: ignore
+            new_status = not guild.strip_buttons
+            guild.strip_buttons = new_status
+            await conn.commit()
+            await conn.close()
+        await ctx.response.send_message(
+            f"Button stripping is now {new_status}",
+            ephemeral=True,
+        )
 
 
 async def setup(bot: TicketsPlus):
