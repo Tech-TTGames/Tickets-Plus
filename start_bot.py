deef6f8ae1896896ae4260cd6ce3609820367e71
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-03-11 13:07:37 +0100

    psycopg fix

--- start_bot.py
+++ start_bot.py
@@ -1,8 +1,13 @@
 """Initiates the bot"""
 import asyncio
+import sys
 
 from tickets_plus import start_bot
 
 if __name__ == "__main__":
+    if (
+        sys.platform == "win32"
+    ):  # Psycopg3 doesn't work on Windows in async mode without this
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_bot())
