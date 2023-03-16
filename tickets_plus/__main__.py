67093faf4e6079a97587ed0a8eb91ece049e7ef8
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-03-16 22:06:03 +0100

    Moved start_bot to __main__.py it's cleaner

--- tickets_plus/__main__.py
+++ tickets_plus/__main__.py
@@ -6,13 +6,13 @@ All the magic happens in `tickets_plus`.
 This file is to be used as a script, not as a module.
 
 Typical usage example:
-    $ python3 start_bot.py
+    $ python3 tickets_plus
     OR
     $ poetry run start
 """
 # License: EPL-2.0
 # SPDX-License-Identifier: EPL-2.0
-# Copyright (c) 2021-present The Tickets Plus Contributors
+# Copyright (c) 2021-present The Tickets+ Contributors
 # This Source Code may also be made available under the following
 # Secondary Licenses when the conditions for such availability set forth
 # in the Eclipse Public License, v. 2.0 are satisfied: GPL-3.0-only OR
