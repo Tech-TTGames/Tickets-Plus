a9b0fe514d254cd7574f9db925c60bd2031bc7d8
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-04-21 12:53:00 +0200

    Missing boilerplate

--- tickets_plus/ext/exceptions.py
+++ tickets_plus/ext/exceptions.py
@@ -13,6 +13,13 @@ Typical usage example:
         ...
     ```
 """
+# License: EPL-2.0
+# SPDX-License-Identifier: EPL-2.0
+# Copyright (c) 2021-present The Tickets+ Contributors
+# This Source Code may also be made available under the following
+# Secondary Licenses when the conditions for such availability set forth
+# in the Eclipse Public License, v. 2.0 are satisfied: GPL-3.0-only OR
+# If later approved by the Initial Contrubotor, GPL-3.0-or-later.
 from discord import app_commands
 
 
