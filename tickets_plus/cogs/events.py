6abfcfa5e316062bf9fe204fb3ea661a5fda9696
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-12-06 09:38:12 +0100

    Update events.py

--- tickets_plus/cogs/events.py
+++ tickets_plus/cogs/events.py
@@ -233,7 +233,7 @@ class Events(commands.Cog, name="Events"):
         """
         chan = message.channel
         if guild.any_autoclose:
-            time_since_update = (datetime.datetime.utcnow() - ticket.last_response)
+            time_since_update = datetime.datetime.utcnow() - ticket.last_response
             if time_since_update >= datetime.timedelta(minutes=5):
                 crrnt = chan.topic  # type: ignore
                 if crrnt is None:
